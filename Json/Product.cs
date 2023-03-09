300d1c8fba7c98eabde239c748668353334cce7a
Vancobombata1 <i.kirilov2019@pmg-pernik.com>
2023-03-09 11:55:34 +0200

    ZadSql

---
+++ Json/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Json
+{
+    public class Product
+    {
+        public Product(int id, string name, decimal price, int stock, DateTime expiry)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Stock = stock;
+            Expiry = expiry;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public DateTime Expiry { get; set; }
+
+    }
+}
---
+++ Json/Program.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace Json
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product(1, "Test product", 100.01m, 100, new DateTime (2019, 06, 30));
+            string json = JsonConvert.SerializeObject(product);
+            Console.WriteLine("Single produut object :");
+            Console.WriteLine(json);
+            Console.WriteLine(new string ('-', 50));
+
+        }
+    }
+}
